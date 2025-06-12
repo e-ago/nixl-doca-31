f0472e4201515e622dbd0c85b1c47e94a499b1eb
Roie Danino <rdanino@nvidia.com>
2025-06-12 11:29:02 +0300

    .GITLAB: set gds_path by CPU arch
    Signed-off-by: Roie Danino <rdanino@nvidia.com>

--- .gitlab/build.sh
+++ .gitlab/build.sh
@@ -106,7 +106,7 @@ export NIXL_PLUGIN_DIR=${INSTALL_DIR}/lib/$ARCH-linux-gnu/plugins
 # UCX transfers and can cause contention with local collectives.
 export UCX_TLS=^cuda_ipc
 
-meson setup nixl_build --prefix=${INSTALL_DIR} -Ducx_path=${UCX_INSTALL_DIR} -Dbuild_docs=true -Denable_rust_bindings=false
+meson setup nixl_build --prefix=${INSTALL_DIR} -Ducx_path=${UCX_INSTALL_DIR} -Dbuild_docs=true -Denable_rust_bindings=false -Dgds_path=/usr/local/cuda/targets/$ARCH-linux/
 cd nixl_build && ninja && ninja install
 
 # TODO(kapila): Copy the nixl.pc file to the install directory if needed.
