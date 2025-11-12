8589307366aa0ab2f8842025395e7518378402bd
eagostini <eagostini@nvidia.com>
2025-11-12 06:43:30 -0800

    Add nixlbench gpunetio to CI test
    Signed-off-by: eagostini <eagostini@nvidia.com>

--- .gitlab/test_nixlbench.sh
+++ .gitlab/test_nixlbench.sh
@@ -100,4 +100,14 @@ for op_type in READ WRITE; do
     done
 done
 
+if $HAS_GPU ; then
+    for op_type in WRITE READ; do
+        for initiator in $seg_types; do
+            for target in $seg_types; do
+                run_nixlbench_two_workers --backend GPUNETIO --device_list=mlx5_0 --gpunetio_oob_list=lo --op_type $op_type --initiator_seg_type $initiator --target_seg_type $target
+            done
+        done
+    done
+fi
+
 pkill etcd
